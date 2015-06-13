f6333a57f06d8f58637e793e7b18a065707738a4
yupog2003 <yupog_2003@yahoo.com.tw>
2015-06-13 17:36:31 +0800

    add confirm dialog

--- disable.js
+++ disable.js
@@ -1,3 +1,9 @@
 var menu=document.getElementById("menu_collapse");
 var dcard=menu.getElementsByTagName("a")[1];
-dcard.style.display = "none";
+//dcard.style.display = "none";
+dcard.setAttribute('href', "#");
+dcard.addEventListener("click", function(){
+    if (confirm("你確定要抽卡嗎？") == true){
+        document.location.href="https://www.dcard.tw/dcard";
+    }
+});
